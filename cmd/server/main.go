4d0be29fe96f9f94b84109bc32fde7ccb64fb2c1
Arthur <baginian.artur@yandex.ru>
2024-11-03 22:02:04 +0300

    Feat: some changes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,10 +8,5 @@ import (
 
 func main() {
 	server := server.NewServer(":3000")
-	// go func() {
-	// 	for msg := range server.msgCh {
-	// 		log.Printf("From %s: %s", msg.From, string(msg.Payload))
-	// 	}
-	// }()
 	log.Fatal(server.Start())
 }
